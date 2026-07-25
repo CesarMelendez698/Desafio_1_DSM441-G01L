6126bd1f75face9953c29aaa1af3849edde2fa62
CesarMelendez <cesararmandomelendez2525@gmail.com>
2026-07-25 15:47:50 -0600

    Activity 1: Pantalla de Bienvenida

--- app/src/main/java/com/example/miperfil/MainActivity.kt
+++ app/src/main/java/com/example/miperfil/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.miperfil
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnIniciar = findViewById<Button>(R.id.btnIniciar)
+        btnIniciar.setOnClickListener {
+            // Ir a la pantalla de registro
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
